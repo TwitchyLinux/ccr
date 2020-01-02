db3741d00a41adcde15e0b5e6e9abc769628c941
Tom <twitchyliquid64@ciphersink.net>
2020-01-02 11:13:40 -0800

    debgen: Generate directory resource targets with source set to a directory generator

--- ccr/debgen_generate.go
+++ ccr/debgen_generate.go
@@ -34,22 +34,25 @@ func mkDebResources(p *d2.Paragraph, d *dpkg.Deb, w io.Writer) error {
 
 		rn := mkUniqueName(exists, r.ResourceName())
 		exists[rn] = true
-		fmt.Fprintf(w, "resource(\n  name = '%s',\n", rn)
+		fmt.Fprintf(w, "resource(\n  name   = '%s',\n", rn)
 		names = append(names, rn)
 		switch r.ResourceKind() {
 		case ResDir:
 			fmt.Fprintf(w, "  parent = '%s',\n", common.DirResourceClass.Path)
 			fmt.Fprintf(w, "  path   = '%s',\n", r.(debDir).Name[1:])
+			fmt.Fprintf(w, "  mode   = '%#o',\n", r.(debDir).Mode)
+			fmt.Fprintf(w, "  source = '%s',\n", "common://generators:dir")
 		case ResStdSo:
 			fmt.Fprintf(w, "  parent = '%s',\n", common.SysLibResourceClass.Path)
 			fmt.Fprintf(w, "  path   = '%s',\n", r.(*debStdSo).Hdr.Name[1:])
+			fmt.Fprintf(w, "  source = '%s_%s',\n", ":debsrc", p.Values["Package"])
 		case ResFile:
 			fmt.Fprintf(w, "  parent = '%s',\n", common.FileResourceClass.Path)
 			fmt.Fprintf(w, "  path   = '%s',\n", r.(*debFile).Hdr.Name[1:])
+			fmt.Fprintf(w, "  source = '%s_%s',\n", ":debsrc", p.Values["Package"])
 		default:
 			fmt.Println(r)
 		}
-		fmt.Fprintf(w, "  source = '%s_%s',\n", ":debsrc", p.Values["Package"])
 		fmt.Fprintf(w, ")\n\n")
 	}
 
