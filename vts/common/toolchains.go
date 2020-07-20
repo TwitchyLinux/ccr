5c30b956bb412862dfad441b4a65dbefba88e7a3
Tom <twitchyliquid64@ciphersink.net>
2020-07-19 19:02:02 -0700

    Implement computed value for gcc version

--- vts/common/toolchains.go
+++ vts/common/toolchains.go
@@ -24,7 +24,7 @@ var (
 		Val: &vts.ComputedValue{
 			ReadWrite: true,
 			InlineScript: []byte(`
-inv = run("/usr/local/go/bin/go", "version")
+inv = run("go", "version")
 spl = inv.output.split(' ')
 if len(spl) < 4 or not spl[2].startswith("go") or spl[2].count(".") < 2:
   broken_assumption("go version output format may have changed")
@@ -34,10 +34,35 @@ return spl[2][2:]
 	}
 )
 
-var GccToolchain = &vts.Toolchain{
-	Path: "common://toolchains:gcc",
-	Name: "gcc",
-	BinaryMappings: map[string]string{
-		"gcc": "/usr/bin/gcc",
-	},
-}
+var (
+	GccToolchain = &vts.Toolchain{
+		Path: "common://toolchains:gcc",
+		Name: "gcc",
+		BinaryMappings: map[string]string{
+			"gcc": "/usr/bin/gcc",
+		},
+		Details: []vts.TargetRef{
+			{Target: GccVersion},
+		},
+	}
+
+	GccVersion = &vts.Attr{
+		Path:   "common://toolchains/version:gcc",
+		Name:   "gcc",
+		Parent: vts.TargetRef{Target: SemverClass},
+		Val: &vts.ComputedValue{
+			InlineScript: []byte(`
+inv = run("gcc", "--version")
+lines = inv.output.split('\n')
+if len(lines) < 2:
+  broken_assumption("gcc --version output format may have changed")
+
+spl = lines[0].split(' ')
+if len(spl) < 3 or spl[0] != 'gcc':
+  broken_assumption("gcc --version output format may have changed")
+
+return spl[-1]
+	`),
+		},
+	}
+)
