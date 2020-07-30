61c6a768c5ddc34297fe3f9756a8dc607935ac1e
Tom <twitchyliquid64@ciphersink.net>
2020-07-30 14:18:22 -0700

    Refactor file layout for ccbuild to better split up methods

---
+++ vts/ccbuild/into_target.go
@@ -0,0 +1,76 @@
+package ccbuild
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/twitchylinux/ccr/vts"
+	"go.starlark.net/starlark"
+)
+
+func toCheckTarget(currentPath string, v starlark.Value) (vts.TargetRef, error) {
+	if a, ok := v.(*vts.Checker); ok {
+		return vts.TargetRef{Target: a}, nil
+	}
+	if s, ok := v.(starlark.String); ok {
+		if strings.HasPrefix(string(s), ":") {
+			return vts.TargetRef{Path: currentPath + string(s)}, nil
+		}
+		return vts.TargetRef{Path: string(s)}, nil
+	}
+	return vts.TargetRef{}, fmt.Errorf("cannot reference check with starklark type %T (%s)", v, v.String())
+}
+
+func toDepTarget(currentPath string, v starlark.Value) (vts.TargetRef, error) {
+	if s, ok := v.(starlark.String); ok {
+		if strings.HasPrefix(string(s), ":") {
+			return vts.TargetRef{Path: currentPath + string(s)}, nil
+		}
+		return vts.TargetRef{Path: string(s)}, nil
+	}
+	if constraint, isConstraint := v.(*RefComparisonConstraint); isConstraint {
+		base, err := toDepTarget(currentPath, constraint.Target)
+		if err != nil {
+			return vts.TargetRef{}, fmt.Errorf("constraint target: %v", err)
+		}
+		base.Constraints = append(base.Constraints, vts.RefConstraint{
+			Meta:   vts.TargetRef{Target: constraint.AttrClass},
+			Params: []starlark.Value{starlark.String(constraint.Op.String()), constraint.CompareValue},
+			Eval:   constraint,
+		})
+		return base, nil
+	}
+	return vts.TargetRef{}, fmt.Errorf("cannot reference dep with starklark type %T (%s)", v, v.String())
+}
+
+func toDetailsTarget(currentPath string, v starlark.Value) (vts.TargetRef, error) {
+	if a, ok := v.(*vts.Attr); ok {
+		return vts.TargetRef{Target: a}, nil
+	}
+	if s, ok := v.(starlark.String); ok {
+		if strings.HasPrefix(string(s), ":") {
+			return vts.TargetRef{Path: currentPath + string(s)}, nil
+		}
+		return vts.TargetRef{Path: string(s)}, nil
+	}
+	return vts.TargetRef{}, fmt.Errorf("cannot reference detail with starklark type %T (%s)", v, v.String())
+}
+
+func toGeneratorTarget(currentPath string, v starlark.Value) (vts.TargetRef, error) {
+	if a, ok := v.(*vts.Generator); ok {
+		return vts.TargetRef{Target: a}, nil
+	}
+	if a, ok := v.(*vts.Build); ok {
+		return vts.TargetRef{Target: a}, nil
+	}
+	if a, ok := v.(*vts.Puesdo); ok && (a.Kind == vts.FileRef || a.Kind == vts.DebRef) {
+		return vts.TargetRef{Target: a}, nil
+	}
+	if s, ok := v.(starlark.String); ok {
+		if strings.HasPrefix(string(s), ":") {
+			return vts.TargetRef{Path: currentPath + string(s)}, nil
+		}
+		return vts.TargetRef{Path: string(s)}, nil
+	}
+	return vts.TargetRef{}, fmt.Errorf("cannot reference generator with starklark type %T (%s)", v, v.String())
+}
---
+++ vts/ccbuild/other_constructors.go
@@ -0,0 +1,162 @@
+package ccbuild
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/twitchylinux/ccr/vts"
+	"go.starlark.net/starlark"
+)
+
+func makeToolchain(s *Script) *starlark.Builtin {
+	t := vts.TargetToolchain
+
+	return starlark.NewBuiltin(t.String(), func(thread *starlark.Thread, fn *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
+		var name string
+		var deps, details *starlark.List
+		var binaries *starlark.Dict
+		if err := starlark.UnpackArgs(t.String(), args, kwargs, "name", &name, "deps?", &deps, "details?", &details, "binaries?", &binaries); err != nil {
+			return starlark.None, err
+		}
+
+		tc := &vts.Toolchain{
+			Path:           s.makePath(name),
+			Name:           name,
+			BinaryMappings: map[string]string{},
+			Pos: &vts.DefPosition{
+				Path:  s.fPath,
+				Frame: thread.CallFrame(1),
+			},
+		}
+		if deps != nil {
+			i := deps.Iterate()
+			defer i.Done()
+			var x starlark.Value
+			for i.Next(&x) {
+				v, err := toDepTarget(s.path, x)
+				if err != nil {
+					return nil, fmt.Errorf("invalid dep: %v", err)
+				}
+				tc.Deps = append(tc.Deps, v)
+			}
+		}
+		if details != nil {
+			i := details.Iterate()
+			defer i.Done()
+			var x starlark.Value
+			for i.Next(&x) {
+				v, err := toDetailsTarget(s.path, x)
+				if err != nil {
+					return nil, fmt.Errorf("invalid detail: %v", err)
+				}
+				tc.Details = append(tc.Details, v)
+			}
+		}
+		if binaries != nil {
+			for _, name := range binaries.Keys() {
+				n, ok := name.(starlark.String)
+				if !ok {
+					return nil, fmt.Errorf("invalid binary key: %v", name)
+				}
+				v, _, err := binaries.Get(name)
+				if err != nil {
+					return starlark.None, nil
+				}
+				v2, ok := v.(starlark.String)
+				if !ok {
+					return nil, fmt.Errorf("invalid binary value: %v", v)
+				}
+				tc.BinaryMappings[string(n)] = string(v2)
+			}
+		}
+
+		s.targets = append(s.targets, tc)
+		return starlark.None, nil
+	})
+}
+
+func makeComputedValue(s *Script) *starlark.Builtin {
+	return starlark.NewBuiltin("compute", func(thread *starlark.Thread, fn *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
+		var fname, fun, code string
+
+		if len(kwargs) == 0 && len(args) == 1 {
+			c, ok := args[0].(starlark.String)
+			if !ok {
+				return starlark.None, fmt.Errorf("compute in unary-argument form must be called with a string, got %T", args[0])
+			}
+			code = string(c)
+		} else {
+			if err := starlark.UnpackArgs("compute", args, kwargs, "path?", &fname, "run?", &fun, "code?", &code); err != nil {
+				return starlark.None, err
+			}
+			fname = filepath.Join(filepath.Dir(s.fPath), fname)
+		}
+
+		wd, _ := os.Getwd()
+		return &vts.ComputedValue{
+			ContractDir:  filepath.Join(wd, filepath.Dir(s.fPath)),
+			ContractPath: s.fPath,
+			Filename:     fname,
+			Func:         fun,
+			InlineScript: []byte(code),
+			Pos: &vts.DefPosition{
+				Path:  s.fPath,
+				Frame: thread.CallFrame(1),
+			},
+		}, nil
+	})
+}
+
+func makePuesdotarget(s *Script, kind vts.PuesdoKind) *starlark.Builtin {
+	t := vts.TargetPuesdo
+
+	return starlark.NewBuiltin(t.String(), func(thread *starlark.Thread, fn *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
+		var path, sha256, url string
+		var name string
+		var host bool
+		var details *starlark.List
+		if err := starlark.UnpackArgs(t.String(), args, kwargs, "path?", &path,
+			"name?", &name, "details?", &details, "host?", &host,
+			"sha256?", &sha256, "url?", &url); err != nil {
+			return starlark.None, err
+		}
+
+		pt := &vts.Puesdo{
+			Kind:         kind,
+			TargetPath:   s.makePath(name),
+			Name:         name,
+			Host:         host,
+			ContractPath: s.fPath,
+			Pos: &vts.DefPosition{
+				Path:  s.fPath,
+				Frame: thread.CallFrame(1),
+			},
+
+			Path:   path,
+			SHA256: sha256,
+			URL:    url,
+		}
+
+		if details != nil {
+			i := details.Iterate()
+			defer i.Done()
+			var x starlark.Value
+			for i.Next(&x) {
+				v, err := toDetailsTarget(s.path, x)
+				if err != nil {
+					return nil, fmt.Errorf("invalid detail: %v", err)
+				}
+				pt.Details = append(pt.Details, v)
+			}
+		}
+
+		// If theres no name, it must be an anonymous target as part of another
+		// target. We don't add it to the targets list.
+		if name == "" {
+			return pt, nil
+		}
+		s.targets = append(s.targets, pt)
+		return starlark.None, nil
+	})
+}
