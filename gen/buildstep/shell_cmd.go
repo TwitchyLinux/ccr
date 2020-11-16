6abd263a44e85868b5ac66a776855c02705d260e
Tom <twitchyliquid64@ciphersink.net>
2020-11-15 22:47:37 -0800

    All shell_cmd build steps: disable hashing and set default umask

--- gen/buildstep/shell_cmd.go
+++ gen/buildstep/shell_cmd.go
@@ -8,6 +8,9 @@ import (
 
 // RunShellCmd runs a shell command in the build environment.
 func RunShellCmd(rb RunningBuild, step *vts.BuildStep, o, e io.Writer) error {
+	if len(step.Args) > 0 {
+		step.Args[0] = "set +h;umask 022;" + step.Args[0]
+	}
 	_, err := rb.ExecBlocking("/tmp", append([]string{"/bin/bash", "-c"}, step.Args...), o, e)
 	return err
 }
